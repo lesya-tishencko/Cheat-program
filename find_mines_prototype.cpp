5900d9acfbf7ae6b8a99630bcf20d3c4f526377e
Mike Koltsov <misha@koltsov.su>
2017-05-04 16:25:02 +0300

    Do alt+tab properly in code

--- find_mines_prototype.cpp
+++ find_mines_prototype.cpp
@@ -1,3 +1,5 @@
+#include "stdafx.h"
+
 #include <comdef.h>
 #include <TlHelp32.h>
 #include <windows.h>
@@ -5,6 +7,36 @@
 #include <iostream>
 
 using namespace std;
+
+
+// Alt-tabbing per http://stackoverflow.com/a/28431897.
+// All other WinApi-related code is based on MSDN.
+void sendkey(INPUT* input, WORD scan_code, bool tf_down_up) {
+	input->type = INPUT_KEYBOARD;
+	input->ki.wScan = scan_code;
+	input->ki.dwFlags = KEYEVENTF_SCANCODE;
+	if (!tf_down_up) input->ki.dwFlags |= KEYEVENTF_KEYUP;
+}
+
+void keydown(INPUT* input, WORD scan_code) {
+	sendkey(input, scan_code, true);
+}
+
+void keyup(INPUT* input, WORD scan_code) {
+	sendkey(input, scan_code, false);
+}
+
+void alt_tab() {
+	const WORD TAB_SCANCODE = 0x0f;
+	const WORD LEFT_ALT_SCANCODE = 0x38;
+	INPUT input[4];
+	keydown(input + 0, LEFT_ALT_SCANCODE);
+	keydown(input + 1, TAB_SCANCODE);
+	keyup(input + 2, TAB_SCANCODE);
+	keyup(input + 3, LEFT_ALT_SCANCODE);
+	SendInput(4, input, sizeof(INPUT));
+}
+
 
 size_t find_minesweeper_pid()
 {
@@ -41,7 +73,6 @@ int main(void) {
 		cout << "Opening process failed with error #" << GetLastError() << endl;
 		return 0;
 	}
-	
 
 	SYSTEM_INFO s_info;
 	GetNativeSystemInfo(&s_info);
@@ -55,6 +86,7 @@ int main(void) {
 	{
 		Sleep(1000);
 		cur_mem_addr = 0;
+		bool found_anything = false;
 
 		while (cur_mem_addr < s_info.lpMaximumApplicationAddress)
 		{
@@ -65,7 +97,8 @@ int main(void) {
 				goto hell;
 			}
 			cur_mem_addr = mem_info.BaseAddress;
-
+			//cout << "Current address: " << cur_mem_addr << endl;
+			//cout << "\tregion size: " << mem_info.RegionSize << endl;
 			uint64_t reg_size = mem_info.RegionSize;
 			uint64_t max_mem = static_cast<uint64_t>(1e8);
 			if (reg_size > max_mem) {
@@ -80,11 +113,11 @@ int main(void) {
 				goto loop_end;
 			}
 
-			const int pat_length = 20;
+			const int pat_length = 17;
 			char pattern[pat_length + 3];
 			memset(pattern, 0, sizeof(pattern));
 			pattern[15] = 1;
-			pattern[16] = 0;
+			pattern[16] = 0; 
 			// Search for 
 			// 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 <-- just indices, for convenience.
 			// y 0 0 0 x 0 0 0 0 0 0  0  0  0  0  1  0
@@ -103,26 +136,28 @@ int main(void) {
 					size_t mem_shifted_idx = idx + shift;
 					if (shift == 0 || shift == 4) // indices of `y` and `x`.
 						continue;
-					if (shift > 16)
-						found_correctly &= buffer[mem_shifted_idx] > 200;
-					else
-						found_correctly &= pattern[shift] == buffer[mem_shifted_idx];
+					found_correctly &= pattern[shift] == buffer[mem_shifted_idx];
 				}
-
 
 				if (found_correctly)
 				{
-					uint64_t cell_addr = idx + (uint64_t)(cur_mem_addr);
-					cout << dec << "Mine in cell x=" << x << ", y=" << y << endl;
-					cout << hex << cell_addr << endl;
 					unsigned char* buffer_write = new unsigned char[17]();
 					memset(buffer_write, 0, 17 * sizeof(*buffer_write));
 					buffer_write[0] = y;
 					buffer_write[4] = x;
 					buffer_write[8] = 1;
 					buffer_write[15] = 1;
-					if (!WriteProcessMemory(proc_handle, (void *)cell_addr, buffer_write, 17, NULL))
-						printf("WriteProcess failed (%d).\n", GetLastError());
+					if (!WriteProcessMemory(proc_handle, (void *)(idx + (uint64_t)cur_mem_addr), buffer_write, 17, NULL))
+					{
+						// Just do nothing: error means that this is false positive.
+						//printf("WriteProcess failed (%d).\n", GetLastError());
+					}
+					else
+					{
+						cout << dec << "Probably a mine in cell x=" << x << ", y=" << y << endl;
+						cout << hex << idx + (uint64_t)(cur_mem_addr) << endl;
+						found_anything = true;
+					}
 					delete[] buffer_write;
 				}
 			}
@@ -132,6 +167,13 @@ int main(void) {
 
 			cur_mem_addr = (LPVOID)((char*)mem_info.BaseAddress +
 				reg_size);
+		}
+
+		if (found_anything)
+		{
+			alt_tab();
+			Sleep(100);
+			alt_tab();
 		}
 	}
 
