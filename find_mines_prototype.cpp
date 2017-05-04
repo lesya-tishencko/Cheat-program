d02ef4a9318feebee86f712752e77e6ff333cd84
Mike Koltsov <misha@koltsov.su>
2017-05-04 14:34:16 +0300

    Impement finding Minesweeper process by name

--- find_mines_prototype.cpp
+++ find_mines_prototype.cpp
@@ -1,4 +1,5 @@
 #include <comdef.h>
+#include <TlHelp32.h>
 #include <windows.h>
 #include <stdio.h>
 #include <iostream>
@@ -6,24 +7,46 @@
 using namespace std;
 
 
+size_t find_minesweeper_pid()
+{
+	HANDLE processes = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	
 
-int main(void) {
-	//freopen("output.txt", "w", stdout);
-	LPWSTR path_to_exe = L"C:\\Users\\lesya\\Downloads\\Minesweeper-master\\Minesweeper-master\\build-minesweeper-Desktop_Qt_5_8_0_MSVC2015_64bit-Debug\\debug\\Minesweeper.exe";
+	PROCESSENTRY32 cur_entry;
+	cur_entry.dwSize = sizeof(cur_entry);
+	size_t result_pid = 0;
 
-	STARTUPINFO info = { sizeof(info) };
-	PROCESS_INFORMATION processInfo;
-	if (!CreateProcess(path_to_exe, L"", NULL, NULL, TRUE, PROCESS_VM_WRITE | PROCESS_VM_OPERATION, NULL, NULL, &info, &processInfo))
+	if (Process32First(processes, &cur_entry)) 
 	{
-		cout << "Failed to start process" << endl;
-		return 1;
+		do
+		{
+			if (wcscmp(cur_entry.szExeFile, L"Minesweeper.exe") == 0) {
+				result_pid = cur_entry.th32ProcessID;
+			}
+		} while (Process32Next(processes, &cur_entry));
 	}
 
+	CloseHandle(processes);
+
+	return result_pid;
+}
+
+
+int main(void) {
+	size_t pid = find_minesweeper_pid();
+
+	HANDLE proc_handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+
+	if (proc_handle == 0) 
+	{
+		cout << "Opening process failed with error #" << GetLastError() << endl;
+		return 0;
+	}
+	
 
 	SYSTEM_INFO s_info;
 	GetNativeSystemInfo(&s_info);
-
-	HANDLE proc_handle = processInfo.hProcess;
+	
 	void* cur_mem_addr = (void*)0;
 
 	int field_width = 50;
@@ -33,6 +56,7 @@ int main(void) {
 	{
 		Sleep(1000);
 		cur_mem_addr = 0;
+
 		while (cur_mem_addr < s_info.lpMaximumApplicationAddress)
 		{
 			MEMORY_BASIC_INFORMATION mem_info;
@@ -42,8 +66,6 @@ int main(void) {
 				goto hell;
 			}
 			cur_mem_addr = mem_info.BaseAddress;
-			//cout << "Current address: " << cur_mem_addr << endl;
-			//cout << "\tregion size: " << mem_info.RegionSize << endl;
 			uint64_t reg_size = mem_info.RegionSize;
 			uint64_t max_mem = static_cast<uint64_t>(1e8);
 			if (reg_size > max_mem) {
@@ -62,12 +84,12 @@ int main(void) {
 			char pattern[pat_length + 3];
 			memset(pattern, 0, sizeof(pattern));
 			pattern[15] = 1;
-			pattern[16] = 0; // TODO: make it zero after debug.
-							 // Search for 
-							 // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 <-- just indices, for convenience.
-							 // y 0 0 0 x 0 0 0 0 0 0  0  0  0  0  1  0
-							 // (17 characters in total).
-							 // If this is found, then a mine is *probably* set at (x, y).
+			pattern[16] = 0; 
+			// Search for 
+			// 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 <-- just indices, for convenience.
+			// y 0 0 0 x 0 0 0 0 0 0  0  0  0  0  1  0
+			// (17 characters in total).
+			// If this is found, then a mine is *probably* set at (x, y).
 			for (size_t idx = 0; idx < reg_size; idx++) {
 				if (idx + pat_length >= reg_size)
 					break;
@@ -86,9 +108,10 @@ int main(void) {
 
 				if (found_correctly)
 				{
-					cout << dec << "Probably mine in cell x=" << x << ", y=" << y << endl;
+					cout << dec << "Probably a mine in cell x=" << x << ", y=" << y << endl;
 					cout << hex << idx + (uint64_t)(cur_mem_addr) << endl;
 					unsigned char* buffer_write = new unsigned char[17]();
+					memset(buffer_write, 0, 17 * sizeof(*buffer_write));
 					buffer_write[0] = y;
 					buffer_write[4] = x;
 					buffer_write[8] = 1;
@@ -108,9 +131,6 @@ int main(void) {
 	}
 
 hell:
-	WaitForSingleObject(processInfo.hProcess, INFINITE);
-	//TerminateProcess(processInfo.hProcess, 0);
-	CloseHandle(processInfo.hProcess);
-	CloseHandle(processInfo.hThread);
+	CloseHandle(proc_handle);
 	return 0;
 }
