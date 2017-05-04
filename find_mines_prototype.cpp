cd67a7b0c5fd25c45ca270b08a0290053aa3c38b
Mike Koltsov <misha@koltsov.su>
2017-05-04 11:22:43 +0300

    Update prototype: traverse all memory automaticaly

--- find_mines_prototype.cpp
+++ find_mines_prototype.cpp
@@ -8,7 +8,9 @@
 using namespace std;
 
 
+
 int main(void) {
+	//freopen("output.txt", "w", stdout);
 	LPWSTR path_to_exe = L"C:\\study\\devdays\\Minesweeper\\build-minesweeper-Desktop_Qt_5_6_2_MSVC2015_64bit-Release\\release\\Minesweeper.exe";
 
 	STARTUPINFO info = { sizeof(info) };
@@ -19,21 +21,7 @@ int main(void) {
 		return 1;
 	}
 
-	/*
-	HMODULE module_handle = GetModuleHandle(path_to_exe);
-	if (!module_handle) 
-	{
-		cout << "Failed to get module handle" << endl;
-		cout << GetLastError() << endl;
-		_com_error err(GetLastError());
-		LPCTSTR errorText = err.ErrorMessage();
-		cout << "err: " << errorText << endl;
-		goto hell;
-	}
-	cout << module_handle << endl;
-	
-	goto hell;
-	*/
+	Sleep(1000);
 	
 
 	SYSTEM_INFO s_info;
@@ -42,15 +30,11 @@ int main(void) {
 	cout << s_info.lpMaximumApplicationAddress << endl;
 
 	HANDLE proc_handle = processInfo.hProcess;
-	void* cur_mem_addr = (void*)0x140000000;
+	void* cur_mem_addr = (void*)0;
 
 	int field_width = 16;
 	int field_height = 30;
 
-	uint64_t val;
-	cin >> hex >> val;
-	cur_mem_addr = (void*)val;
-	cout << cur_mem_addr << endl;
 
 	while (cur_mem_addr < s_info.lpMaximumApplicationAddress)
 	{
@@ -61,59 +45,57 @@ int main(void) {
 			goto hell;
 		}
 		cur_mem_addr = mem_info.BaseAddress;
-		cout << "Current address: " << cur_mem_addr << endl;
-		cout << "\tregion size: " << mem_info.RegionSize << endl;
+		//cout << "Current address: " << cur_mem_addr << endl;
+		//cout << "\tregion size: " << mem_info.RegionSize << endl;
 		uint64_t reg_size = mem_info.RegionSize;
 		uint64_t max_mem = static_cast<uint64_t>(1e8);
 		if (reg_size > max_mem) {
 			reg_size = max_mem;
 		}
 
-		char* buffer = new char[reg_size];
+		unsigned char* buffer = new unsigned char[reg_size];
 		
 		if (!ReadProcessMemory(proc_handle, cur_mem_addr, buffer, reg_size, NULL))
 		{
-			cout << "Failed to read process memory" << endl;
-			cout << GetLastError() << endl;
-			goto hell;
+			reg_size = mem_info.RegionSize;
+			goto loop_end;
 		}
 
-		for (int idx = 0; idx < reg_size; idx++) {
-			const int pat_length = 17;
-			char pattern[pat_length + 3];
-			memset(pattern, 0, sizeof(pattern));
+		const int pat_length = 17;
+		char pattern[pat_length + 3];
+		memset(pattern, 0, sizeof(pattern));
+		pattern[15] = 1;
+		pattern[16] = 1; // TODO: make it zero after debug.
+		// Search for 
+		// 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 <-- just indices, for convenience.
+		// y 0 0 0 x 0 0 0 0 0 0  0  0  0  0  1  0
+		// (17 characters in total).
+		// If this is found, then a mine is *probably* set at (x, y).
+		for (size_t idx = 0; idx < reg_size; idx++) {
+			if (idx + pat_length >= reg_size)
+				break;
 
-			for (int y = 1; y <= field_height; y++) {
-				for (int x = 1; x <= field_width; x++) {
-					// Search for 
-					// 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 <-- just indicies, for convenience.
-					// y 0 0 0 x 0 0 0 0 0 0  0  0  0  0  1  0
-					// (17 characters in total).
-					// If this is found, then a mine is *probably* set at (x, y).
-					pattern[0] = y;
-					pattern[4] = x;
-					pattern[15] = 1;
-					pattern[16] = 0; // TODO: make it zero after debug.
-					bool found_correctly = true;
-					for (int shift = 0; found_correctly && shift < pat_length; shift++) {
-						int mem_shifted_idx = idx + shift;
-						if (mem_shifted_idx >= reg_size) {
-							found_correctly = false;
-							break;
-						}
-						found_correctly &= pattern[shift] == buffer[mem_shifted_idx];
-					}
+			unsigned y = buffer[idx];
+			unsigned x = buffer[idx + 4];
 
-					if (found_correctly)
-					{
-						// TODO: we want to find actual mines, not "no mines".
-						cout << dec << "Probably no mine in cell x=" << x << ", y=" << y << endl;
-						cout << hex << idx + (uint64_t)(cur_mem_addr) << endl;
-					}
-				}
+			bool found_correctly = (1 <= x && x <= field_width) && (1 <= y && y <= field_height);
+
+			for (size_t shift = 0; found_correctly && shift < pat_length; shift++) {
+				size_t mem_shifted_idx = idx + shift;
+				if (shift == 0 || shift == 4) // indices of `y` and `x`.
+					continue;
+				found_correctly &= pattern[shift] == buffer[mem_shifted_idx];
+			}
+
+			if (found_correctly)
+			{
+				// TODO: we want to find actual mines, not "no mines".
+				cout << dec << "Probably no mine in cell x=" << x << ", y=" << y << endl;
+				cout << hex << idx + (uint64_t)(cur_mem_addr) << endl;
 			}
 		}
 
+loop_end:
 		delete[] buffer;
 
 		cur_mem_addr = (LPVOID)((char*)mem_info.BaseAddress +
